3868b6ab73d36d74e18940955fcd609f234c1682
Gabriel <gabrielx165@gmail.com>
2022-06-21 10:01:37 -0300

    defined table on leads page

--- src/Pages/Leads.jsx
+++ src/Pages/Leads.jsx
@@ -1,3 +1,21 @@
+import { Spinner } from 'react-bootstrap'
+import { useFetch } from '../hooks/useFetch'
+import { Table } from '../components/Table'
 export function Leads() {
-    return <h1>Leads page</h1>
+    const { data, loading } = useFetch('http://localhost:3001/leads?limit=10&page=1')
+    return (
+        <>
+            <h1>Leads</h1>
+            {loading && (
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            )}
+            {!loading && (
+                < Table
+                    fields={['name', 'email', 'phone', 'mostViewedDestination']}
+                    rowsData={data.results} />
+            )}
+        </>
+    )
 }
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import './Table.css'
 
 export function Table({ fields, rowsData }) {
@@ -5,15 +6,16 @@ export function Table({ fields, rowsData }) {
         <table className="general-table">
             <thead>
                 <tr>
-                    {fields.map((field, i) => <th key={i + 1} className="table-header">{field}</th>)}
+                    {fields.map((field, i) => <th key={uuidv4()} className="table-header">{field}</th>)}
                 </tr>
             </thead>
             <tbody>
                 {rowsData.map(row => {
-                    return (<tr className="table-row" key={row.id}>
+                    return (<tr className="table-row" key={uuidv4()}>
                         {Object.keys(row).map((key) => {
-                            return fields.includes(key) && <td key={row[key]}>{row[key]}</td>
-
+                            return fields.includes(key) && (
+                                row[key] === "" ? <td key={uuidv4()}><p>------</p></td> : <td key={uuidv4()}>{row[key]}</td>
+                            )
                         })}
                     </tr>)
                 })}
