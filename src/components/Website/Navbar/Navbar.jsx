e4ce3b886c45a13369fc8b978a29d8d7a1134d1f
Gabriel Mendes <gabrielx165@gmail.com>
2022-08-29 13:58:31 -0300

    update text of navbar links

--- src/components/Website/Navbar/Navbar.jsx
+++ src/components/Website/Navbar/Navbar.jsx
@@ -1,40 +1,50 @@
-import './Navbar.css'
-import logo from './img/logo-crm-grande.png';
+import "./Navbar.css";
+import logo from "./img/logo-crm-grande.png";
 
-export function Navbar(){
-    return <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand" href="./index.html"
-        ><img src={logo} alt="Logo viaja-tech"
-      /></a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarResponsive"
-        aria-controls="navbarResponsive"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        Menu
-        <i class="fas fa-bars ms-1"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="#services">Nossos Serviços</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#contact">Contact</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./login.html">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./register.html">Register</a>
-          </li>
-        </ul>
+export function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="./index.html">
+          <img src={logo} alt="Logo viaja-tech" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+          <i class="fas fa-bars ms-1"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#services">
+                Nossos Serviços
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">
+                Contato
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./login.html">
+                Login
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./register.html">
+                Registre-se
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  );
 }
