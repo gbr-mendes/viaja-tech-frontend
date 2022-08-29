9c98cdde87d34eb0e1979682669daa1f9a2f2467
Gabriel Mendes <gabrielx165@gmail.com>
2022-08-29 12:21:16 -0300

    Defined services section website component

--- src/Pages/Website/IndexPage/IndexPage.jsx
+++ src/Pages/Website/IndexPage/IndexPage.jsx
@@ -1,12 +1,14 @@
 import "./IndexPage.css";
 import { Masthead } from "../../../components/Website/Masthead/Masthead";
 import { Navbar } from "../../../components/Website/Navbar";
+import { ServicesSection } from "../../../components/Website/ServicesSection/ServicesSection";
 
 export function IndexPage() {
   return (
     <>
       <Navbar />
       <Masthead />
+      <ServicesSection />
     </>
   );
 }
---
+++ src/components/Website/ServicesSection/ServicesSection.jsx
@@ -0,0 +1,34 @@
+import "./ServicesSection.css";
+export function ServicesSection() {
+  return (
+    <section class="page-section" id="services">
+      <div class="container">
+        <div class="text-center">
+          <h2 class="section-heading text-uppercase">Services</h2>
+          <h3 class="section-subheading text-muted">
+            Lorem ipsum dolor sit amet consectetur.
+          </h3>
+        </div>
+        <div class="row text-center">
+          <div class="col-md-12">
+            <span class="fa-solid fa-plane-departure fa-4x"></span>
+            <h4 class="my-3">Lorem Ipsum</h4>
+            <p class="text-muted">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              blanditiis iusto laboriosam saepe, in excepturi, quae ipsum odit
+              quisquam praesentium alias accusantium voluptates voluptatibus
+              esse minima quam molestias? Perferendis, quod. Libero vel quis
+              magnam corrupti necessitatibus. Aperiam unde dignissimos eum
+              nulla? Aut ratione labore aperiam illo voluptates nostrum,
+              voluptatum iste voluptatem omnis a explicabo optio vero obcaecati?
+              Magnam, alias nostrum! Eaque quo voluptate nostrum totam
+              doloremque harum fugit, a laboriosam pariatur at nisi sunt
+              reprehenderit explicabo sit minima ipsam? Odio illum quia saepe!
+              Totam quis vitae corporis tempore ipsa hic?
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
