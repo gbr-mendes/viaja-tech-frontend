ea74403cf233e7659acea8be6b48d812b0e032e4
Gabriel Mendes <gabrielx165@gmail.com>
2022-09-13 11:42:58 -0300

    added magin top to toggle button on dashboard

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -1,13 +1,11 @@
-import { SearchBar } from "./SearchBar";
-import { ToggleBtn } from './ToggleBtn'
+import { ToggleBtn } from "./ToggleBtn";
 
-import './MainHeader.css'
+import "./MainHeader.css";
 
 export function MainHeader() {
-    return (
-        <header className="main-header d-flex flex-column align-items-center p-2">
-            <SearchBar />
-            <ToggleBtn />
-        </header>
-    )
+  return (
+    <header className="main-header d-flex flex-column align-items-center p-2 mt-3">
+      <ToggleBtn />
+    </header>
+  );
 }
