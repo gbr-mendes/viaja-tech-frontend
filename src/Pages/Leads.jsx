0ea163f473208675762a4ef763e31129af1dd37c
Gabriel Mendes <gabrielx165@gmail.com>
2022-09-13 09:52:56 -0300

    defined modal to show wen an element of the leads list is cliecked

--- src/Pages/Leads.jsx
+++ src/Pages/Leads.jsx
@@ -1,14 +1,43 @@
 import { Spinner } from "react-bootstrap";
 import { useFetch } from "../hooks/useFetch";
 import { Table } from "../components/Table";
+import { useState } from "react";
+import { useEffect } from "react";
+import { ReadLeadModal } from "../components/Modals/ReadLeadModal/ReadLeadModal";
+import { fetchGet } from "../utils/FetchGet";
+
 export function Leads() {
   const authToken = localStorage.getItem("auth-token");
+  const [idFetchElement, setIdFetchElement] = useState(null);
+  const [show, setShow] = useState(true);
+  const [modalData, setModalData] = useState(null);
   const { data, loading } = useFetch(
     `${process.env.REACT_APP_API_DOAMIN}/leads`,
     authToken
   );
+  const baseUrl = `${process.env.REACT_APP_API_DOAMIN}/leads`;
+  const handleShow = (e) => {
+    setShow(true);
+  };
+  useEffect(() => {
+    async function fetchData() {
+      if (idFetchElement) {
+        const data = await fetchGet(`${baseUrl}/${idFetchElement}`, authToken);
+        console.log(data);
+        setModalData(data);
+        handleShow();
+      }
+    }
+    fetchData();
+  }, [idFetchElement, authToken, baseUrl]);
   return (
     <>
+      <ReadLeadModal
+        show={show}
+        onHide={() => setShow(false)}
+        title="teste"
+        data={modalData}
+      />
       <div className="d-flex justify-content-center">
         <h1>Leads</h1>
       </div>
@@ -21,6 +50,7 @@ export function Leads() {
         <Table
           fields={["name", "email", "phone", "mostViewedDestination"]}
           rowsData={data.results}
+          setIdMethod={setIdFetchElement}
         />
       )}
     </>
---
+++ src/components/Modals/ReadLeadModal/ReadLeadModal.jsx
@@ -0,0 +1,75 @@
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import ListGroup from "react-bootstrap/ListGroup";
+
+export function ReadLeadModal(props) {
+  const formData = props.data || {};
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {formData.name}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3 d-flex">
+            <div className="d-flex flex-column col-6 me-1">
+              <Form.Label>Nome</Form.Label>
+              <Form.Control type="text" value={formData.name} disabled />
+            </div>
+            <div className="d-flex flex-column col-6 ms-1">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" value={formData.email} disabled />
+            </div>
+          </Form.Group>
+          <Form.Group className="mb-3 d-flex">
+            <div className="d-flex flex-column col-6 me-1">
+              <Form.Label>CPF</Form.Label>
+              <Form.Control type="text" value={formData.cpf} disabled />
+            </div>
+            <div className="d-flex flex-column col-6 ms-1">
+              <Form.Label>Telefone</Form.Label>
+              <Form.Control type="email" value={formData.phone} disabled />
+            </div>
+          </Form.Group>
+          <Form.Group className="mb-3 d-flex">
+            <div className="d-flex flex-column col-6 me-1">
+              <Form.Label>Qtd. de visitas ao website</Form.Label>
+              <Form.Control
+                type="number"
+                value={formData.websiteVisits}
+                disabled
+              />
+            </div>
+            <div className="d-flex flex-column col-6 ms-1">
+              <Form.Label>Destino mais visualizado</Form.Label>
+              <Form.Control
+                type="email"
+                value={formData.mostViewedDestination || "--------------"}
+                disabled
+              />
+            </div>
+          </Form.Group>
+          <Form.Group className="mb-3 d-flex">
+            <div className="d-flex flex-column col-12 me-1">
+              <Form.Label>Destinos pesquisados</Form.Label>
+              <ListGroup>
+                {formData.destinationsViewed.length > 0
+                  ? formData.destinationsViewed.map((destination) => {
+                      return <ListGroup.Item>{destination}</ListGroup.Item>;
+                    })
+                  : "--------------"}
+              </ListGroup>
+            </div>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+}
